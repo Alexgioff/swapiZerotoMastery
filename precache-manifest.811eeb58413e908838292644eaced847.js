710034f947cefeab8a42dc5b34f4cad33a5ddc11
Alexgioff <gioffre.alex@outlook.it>
2019-02-13 18:46:34 +0100

    Updates

---
+++ precache-manifest.811eeb58413e908838292644eaced847.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1783cb0d898033ab5f4",
+    "url": "/swapiZerotoMastery/static/css/main.ecdff77e.chunk.css"
+  },
+  {
+    "revision": "a1783cb0d898033ab5f4",
+    "url": "/swapiZerotoMastery/static/js/main.a1783cb0.chunk.js"
+  },
+  {
+    "revision": "403ac9406d1ca2ce0b03",
+    "url": "/swapiZerotoMastery/static/js/runtime~main.403ac940.js"
+  },
+  {
+    "revision": "1e9b66cef695bdd61415",
+    "url": "/swapiZerotoMastery/static/js/2.1e9b66ce.chunk.js"
+  },
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/swapiZerotoMastery/static/media/STARWARS.35f31c7a.woff"
+  },
+  {
+    "revision": "fa7a31d20cbd5f199216f667dfe9c5d8",
+    "url": "/swapiZerotoMastery/static/media/STARWARS.fa7a31d2.eot"
+  },
+  {
+    "revision": "3f085be9c96d464c8ed2774e89793393",
+    "url": "/swapiZerotoMastery/static/media/STARWARS.3f085be9.ttf"
+  },
+  {
+    "revision": "95e43dd634fa7abc1f0bac8278faf5e4",
+    "url": "/swapiZerotoMastery/static/media/STARWARS.95e43dd6.svg"
+  },
+  {
+    "revision": "efda1c2b9787e6e669a7c38a4cd11671",
+    "url": "/swapiZerotoMastery/index.html"
+  }
+];
